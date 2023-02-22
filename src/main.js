5a72a96e7eafb16921117f0f973825e88667139f
Lizet Peche <lizet.peche@gmail.com>
2023-02-22 11:16:25 -1100

    Se creo funcion para mostrar la data por imagen

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//se importa la dat de la corresponiente ruta
+import dataPokemon from "./data/pokemon/pokemon.js";
+//Enviar solicitud al servideor para obtener archivo json. y como argumento tendra el nombre del archivo
 
-console.log(example, data);
+function mostrarData(pokemon) {
+  const placeholder = document.getElementById("data");
+
+  pokemon.forEach((diccionario) => {
+    const image = document.createElement("img"); //<img>
+    image.src = diccionario["img"];
+    placeholder.appendChild(image); // <div id="data-output"> <img  src="linkdeimagen"> <img  src="linkdeimagen"> <img  src="linkdeimagen"> <img  src="linkdeimagen"> </div>
+  });
+}
+mostrarData(dataPokemon.pokemon);
+
