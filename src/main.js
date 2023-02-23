98be07e2b39aa9e477fa93ce10bc880cc8ab3be9
Lizet Peche <lizet.peche@gmail.com>
2023-02-22 20:44:38 -1100

    tarjetas modificadas y ordenadas

--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
 //se importa la dat de la corresponiente ruta
 import dataPokemon from "./data/pokemon/pokemon.js";
-
 const cortarData = dataPokemon.pokemon.slice(0,10);
-
 function mostrarData(pokemones) {
   const data = document.getElementById("data");
-
   pokemones.forEach((pokemon) => {
-    const card =document.createElement("div");
-    card.innerHTML= pokemon["name"];
-    const numbers=document.createElement("number");
+    const card = document.createElement("div");
+    const name = document.createElement("name");
+    name.innerHTML= pokemon["name"];
+    const numbers = document.createElement("number");
     numbers.innerHTML= pokemon["num"];
-    const categoria=document.createElement("type");
+    const categoria = document.createElement("type");
     categoria.innerHTML= pokemon["type"];
     const image = document.createElement("img"); //<img>
     image.src = pokemon["img"];
-    card.appendChild(categoria);
+    card.appendChild(image);
     card.appendChild(numbers);
-    card.appendChild(image); 
+    card.appendChild(name);
+    card.appendChild(categoria);
     data.appendChild(card);
   });
 }
