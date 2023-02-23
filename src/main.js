e6a18fe200f1e4419005973e3c790f0d016fa7f1
Lizet Peche <lizet.peche@gmail.com>
2023-02-22 18:42:19 -1100

    Data cortada y diseñada en css

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 //se importa la dat de la corresponiente ruta
 import dataPokemon from "./data/pokemon/pokemon.js";
-//Enviar solicitud al servideor para obtener archivo json. y como argumento tendra el nombre del archivo
 
-//const container = document.getElementById('container');
-//const poke_number = 251
+const cortarData = dataPokemon.pokemon.slice(0,10);
+
 function mostrarData(pokemones) {
   const data = document.getElementById("data");
 
@@ -12,12 +11,15 @@ function mostrarData(pokemones) {
     card.innerHTML= pokemon["name"];
     const numbers=document.createElement("number");
     numbers.innerHTML= pokemon["num"];
+    const categoria=document.createElement("type");
+    categoria.innerHTML= pokemon["type"];
     const image = document.createElement("img"); //<img>
     image.src = pokemon["img"];
+    card.appendChild(categoria);
     card.appendChild(numbers);
     card.appendChild(image); 
     data.appendChild(card);
   });
 }
-mostrarData(dataPokemon.pokemon);
+mostrarData(cortarData);
 
