313381b9195a1fc30a32e6998376ce83e559ed85
Gvoisier <profesora.gvoisier@gmail.com>
2023-02-23 11:10:35 -0300

    OH luna

--- src/main.js
+++ src/main.js
@@ -1,24 +1,30 @@
 //se importa la dat de la corresponiente ruta
 import dataPokemon from "./data/pokemon/pokemon.js";
-const cortarData = dataPokemon.pokemon.slice(0,10);
-function mostrarData(pokemones) {
-  const data = document.getElementById("data");
+//const cortarData = dataPokemon.pokemon.slice(0,10);
+console.log(dataPokemon.pokemon);
+const divTarjeta = document.getElementById("data");
+console.log(divTarjeta)
+
+const mostrarData = (pokemones) => {
+  divTarjeta.innerHTML = "";
   pokemones.forEach((pokemon) => {
+
     const card = document.createElement("div");
     const name = document.createElement("name");
-    name.innerHTML= pokemon["name"];
-    const numbers = document.createElement("number");
-    numbers.innerHTML= pokemon["num"];
-    const categoria = document.createElement("type");
-    categoria.innerHTML= pokemon["type"];
+    name.innerHTML= pokemon.name;
+    const numbers = document.createElement("numb");
+    numbers.innerHTML= pokemon.num;
+    const type = document.createElement("type");
+    type.innerHTML= pokemon.type;
     const image = document.createElement("img"); //<img>
-    image.src = pokemon["img"];
+    image.setAttribute("src", pokemon.img); //image.src = 
+    divTarjeta.appendChild(card);
     card.appendChild(image);
     card.appendChild(numbers);
     card.appendChild(name);
-    card.appendChild(categoria);
-    data.appendChild(card);
+    card.appendChild(type);
   });
 }
-mostrarData(cortarData);
 
+mostrarData(dataPokemon.pokemon);
+console.log(mostrarData(dataPokemon.pokemon));
