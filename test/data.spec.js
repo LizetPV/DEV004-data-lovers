24908e51fb3d693395e8f037163ffd94e8a1ff1e
Gvoisier <profesora.gvoisier@gmail.com>
2023-03-07 22:38:57 -0300

    Botón Select con diseño CSS

--- test/data.spec.js
+++ test/data.spec.js
@@ -2,12 +2,13 @@
 import { filter, ordenar } from "../src/data.js";
 
 
+// Pruebas unitarias para el "filter"
 describe('Filter', () => {
-  // funcion existe
+  //1° criterio para las pruebas: la función existe
   it('filter deberia ser una funcion', () => {
     expect(typeof filter).toBe('function')
   })
-  // comportamiento deseado de la funcion
+  //2° criterio para las pruebas: validar el comportamiento deseado de la funcion
   const data = [{
     "num": "001",
     "name": "bulbasaur",
@@ -53,7 +54,7 @@ describe('Filter', () => {
       { num: '003', name: 'venusaur', type: ['grass', 'poison'] }
     ])
   })
-  // estrese la funcion
+  //3° criterio para las pruebas: estresar la función
   it('filter deberia retornar algo', () => {
     // expect(filter('', data)).toBe(false)
     expect(filter('', data)).toBeFalsy()
@@ -63,16 +64,8 @@ describe('Filter', () => {
 });
 
 
-// describe('anotherExample', () => {
-//   it('is a function', () => {
-//     expect(typeof anotherExample).toBe('function');
-//   });
 
-//   it('returns `anotherExample`', () => {
-//     expect(anotherExample()).toBe('OMG');
-//   });
-// });
-
+// Pruebas unitarias para "ordenar"
 describe('Ordenar', () => {
   // funcion existe
   it('Ordenar los pokemones alfabéticamente Ascendente', () => {
