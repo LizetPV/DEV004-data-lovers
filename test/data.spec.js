e9d4988d7378e30acb4f5307f11c3082fa8f548e
Gvoisier <profesora.gvoisier@gmail.com>
2023-03-13 17:40:26 -0300

    Realizando pruebas a función de Calcular

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,5 +1,5 @@
 //import { example, anotherExample } from '../src/data.js';
-import { filter, ordenar } from "../src/data.js";
+import { filter, pokemonesPromedio } from "../src/data.js";
 
 
 // Pruebas unitarias para el "filter"
@@ -69,32 +69,49 @@ describe('Filter', () => {
 //     expect(anotherExample()).toBe('OMG');
 //   });
 // });
-describe('Ordenar', () => {
+/*describe('Ordenar', () => {
   // funcion existe
   it('Ordenar los pokemones alfabéticamente Ascendente', () => {
     expect(typeof ordenar).toBe('function')
   })
   // comportamiento deseado de la funcion
   const dataPokemon = [
-    {'name': 'venusaur'},
-    {'name': 'ivysaur'}, 
-    {'name': 'charmeleon'},   
-    {'name': 'charmander'}, 
-    {'name': 'bulbasaur'}]
+    { 'name': 'venusaur' },
+    { 'name': 'ivysaur' },
+    { 'name': 'charmeleon' },
+    { 'name': 'charmander' },
+    { 'name': 'bulbasaur' }]
 
   it('ordenar deberia organizar a los pokemones de forma ascendente', () => {
     expect(ordenar('name', dataPokemon)).toStrictEqual([
-      {'name': 'venusaur'},
-      {'name': 'ivysaur'}, 
-      {'name': 'charmeleon'},
-      {'name': 'charmander'},
-      {'name': 'bulbasaur'} ])
+      { 'name': 'venusaur' },
+      { 'name': 'ivysaur' },
+      { 'name': 'charmeleon' },
+      { 'name': 'charmander' },
+      { 'name': 'bulbasaur' }])
   })
 })
 
 // estrese la funcion
-it('Ordenar deberia retornar algo',()=>{
+it('Ordenar deberia retornar algo', () => {
   // expect(filter('', data)).toBe(false)
   expect(ordenar('', dataPokemon)).toBeFalsy()
   expect(ordenar('water', [])).toBeFalsy()
+})*/
+
+
+describe('pokemonesPromedio', () => {
+  it('calcula correctamente el promedio y ordena los pokemones', () => {
+    expect(pokemonesPromedio(data)).toEqual(resultadoEsperado);
+  })
+  const data = [
+    {"num": "001", "name": "bulbasaur", "stats": {"base-attack": "118", "base-defense": "111", "base-stamina": "128" } },
+    {"num": "002", "name": "ivysaur","stats": {"base-attack": "151","base-defense": "143", "base-stamina": "155"}},
+    {"num": "003", "name": "venusaur","stats": {"base-attack": "198","base-defense": "189","base-stamina": "190"}}
+  ]
+  const resultadoEsperado = [
+    {"num": "001", "name": "bulbasaur", "stats": {"base-attack": "118", "base-defense": "111", "base-stamina": "128" } },
+    {"num": "002", "name": "ivysaur","stats": {"base-attack": "151","base-defense": "143", "base-stamina": "155"}},
+    {"num": "003", "name": "venusaur","stats": {"base-attack": "198","base-defense": "189","base-stamina": "190"}}
+  ]
 })
